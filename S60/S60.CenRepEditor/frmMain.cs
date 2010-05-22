7572879392f278ac187007023145e95dbda0aa10
fonix232 <fonix232@gmail.com>
2010-05-22 17:56:38 +0000

    Added property editor for further use.

--- S60/S60.CenRepEditor/frmMain.cs
+++ S60/S60.CenRepEditor/frmMain.cs
@@ -9,11 +9,17 @@ using System.Windows.Forms;
 
 namespace S60.CenRepEditor
 {
-    public partial class Form1 : Form
+    public partial class frmMain : Form
     {
-        public Form1()
+        public frmMain()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            frmProperties prop = new frmProperties();
+            prop.Show();
         }
     }
 }
