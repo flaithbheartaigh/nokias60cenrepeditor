d9525eee5635dcfced1e1a054a64194ed684c5da
fonix232@gmail.com <fonix232@gmail.com@f7e02285-a4bd-d62b-a74b-6d5bd8df82f2>
2010-05-22 18:04:06 +0000

    Added proper plugin handling to the Plugin Manager

--- trunk/S60/S60.FirmEditor/frmMain.cs
+++ trunk/S60/S60.FirmEditor/frmMain.cs
@@ -11,107 +11,108 @@ using S60.PluginInterface;
 using System.Text.RegularExpressions;
 using S60.FirmEditor;
 using System.Diagnostics;
-using S60.PluginInterface;
-using System.Text.RegularExpressions;
 
 namespace S60.FirmEditor
 {
-  public partial class frmMain : Form
-  {
-    private List<PluginHost> mPlugins;
-    public frmMain()
+    public partial class frmMain : Form
     {
-      InitializeComponent();
-      Utils.Check();
-      mPlugins = PluginHostProvider.Plugins;
-      foreach (PluginHost loadedPlugins in mPlugins)
-      {
-        string strMenuName = loadedPlugins.GetMenuName();
-        string[] strNames = Regex.Split(strMenuName, "/");
-        if (strNames.Length < 2)
-          UpdateMenu("Plugins", strNames[0], loadedPlugins.myHandler);
-        else
-          UpdateMenu(strNames[0], strNames[1], loadedPlugins.myHandler);
-      }
-    }
 
-    private void UpdateMenu(string strParent, string txt, EventHandler cmdHandler)
-    {
-      ToolStripMenuItem dynamicMenu = new ToolStripMenuItem();
-      
-      dynamicMenu.Text = txt;
-      dynamicMenu.Click += cmdHandler;
+        private List<PluginHost> mPlugins;
 
-      foreach (ToolStripMenuItem parent in mnuMain.Items)
-      {
-        if (parent.Text == strParent)
+        public frmMain()
         {
-          parent.DropDownItems.Add(dynamicMenu);
-          return;
+            InitializeComponent();
             Utils.Check();
+            mPlugins = PluginHostProvider.Plugins;
+            foreach (PluginHost loadedPlugins in mPlugins)
+            {
+                string strMenuName = loadedPlugins.GetMenuName();
+                string[] strNames = Regex.Split(strMenuName, "/");
+                if (strNames.Length < 2)
+                    UpdateMenu("Plugins", strNames[0], loadedPlugins.myHandler);
+                else
+                    UpdateMenu(strNames[0], strNames[1], loadedPlugins.myHandler);
+            }
         }
-      }
-      ToolStripMenuItem newDropDown = new ToolStripMenuItem(strParent);
-      newDropDown.DropDownItems.Add(dynamicMenu);
-      mnuMain.Items.Add(newDropDown);
-    }
 
-    private void searchBox_TextChanged(object sender, EventArgs e)
-    {
-      listView.Items.Clear();
-      DirectoryInfo dir = new DirectoryInfo(Application.StartupPath + "\\");
-
-      foreach (DirectoryInfo subDir in dir.GetDirectories("*", SearchOption.AllDirectories))
-      {
-        if (subDir.Name.Contains(searchBox.Text))
+        private void UpdateMenu(string strParent, string txt, EventHandler cmdHandler)
         {
-          ListViewItem item = new ListViewItem(new string[] { subDir.FullName.Substring(Application.StartupPath.Length + 1), "", dir.CreationTime.ToString(), "Folder", "0x0" });
-          listView.Items.Add(item);
+            ToolStripMenuItem dynamicMenu = new ToolStripMenuItem();
+
+            dynamicMenu.Text = txt;
+            dynamicMenu.Click += cmdHandler;
+
+            foreach (ToolStripMenuItem parent in mnuMain.Items)
+            {
+                if (parent.Text == strParent)
+                {
+                    parent.DropDownItems.Add(dynamicMenu);
+                    return;
+                    Utils.Check();
+                }
+            }
+            ToolStripMenuItem newDropDown = new ToolStripMenuItem(strParent);
+            newDropDown.DropDownItems.Add(dynamicMenu);
+            mnuMain.Items.Add(newDropDown);
         }
-      }
-      foreach (FileInfo file in dir.GetFiles("*", SearchOption.AllDirectories))
-      {
-        if (file.Name.Contains(searchBox.Text))
-      {
-        if (!searchBox.Items.Contains(searchBox.Text))
+
+        private void searchBox_TextChanged(object sender, EventArgs e)
         {
-          ListViewItem item = new ListViewItem(new string[] { file.Name, file.Length / 1024 + " kB", file.CreationTime.ToString(), file.GetType().ToString(), "0x0" });
-          listView.Items.Add(item);
+            listView.Items.Clear();
+            DirectoryInfo dir = new DirectoryInfo(Application.StartupPath + "\\");
+
+            foreach (DirectoryInfo subDir in dir.GetDirectories("*", SearchOption.AllDirectories))
+            {
+                if (subDir.Name.Contains(searchBox.Text))
+                {
+                    ListViewItem item = new ListViewItem(new string[] { subDir.FullName.Substring(Application.StartupPath.Length + 1), "", dir.CreationTime.ToString(), "Folder", "0x0" });
+                    listView.Items.Add(item);
+                }
+            }
+            foreach (FileInfo file in dir.GetFiles("*", SearchOption.AllDirectories))
+            {
+                if (file.Name.Contains(searchBox.Text))
+                {
+                    if (!searchBox.Items.Contains(searchBox.Text))
+                    {
+                        ListViewItem item = new ListViewItem(new string[] { file.Name, file.Length / 1024 + " kB", file.CreationTime.ToString(), file.GetType().ToString(), "0x0" });
+                        listView.Items.Add(item);
+                    }
+                }
+
+            }
         }
-      }
 
-      }}
-
-     private void pluginManagerToolStripMenuItem_Click(object sender, EventArgs e)
-    {
-      frmPluginManager pluginMan = new frmPluginManager();
-      pluginMan.Show();
-    }
-
-    private void searchButton_Click(object sender, EventArgs e)
-    {
-      searchBox.Text = "";
-    }
-
-    private void searchBox_KeyPress(object sender, KeyPressEventArgs e)
-    {
-      if (e.KeyChar == (char)13)
-      {
-        if (!searchBox.Items.Contains(searchBox.Text))
+        private void pluginManagerToolStripMenuItem_Click(object sender, EventArgs e)
         {
-          searchBox.Items.Add(searchBox.Text);
+            frmPluginManager pluginMan = new frmPluginManager();
+            pluginMan.Show();
         }
-      }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            searchBox.Text = "";
+        }
+
+        private void searchBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)
+            {
+                if (!searchBox.Items.Contains(searchBox.Text))
+                {
+                    searchBox.Items.Add(searchBox.Text);
+                }
+            }
+        }
+
+        private void exitFEdToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            Application.Exit();
+        }
+
     }
 
-    private void exitFEdToolStripMenuItem_Click(object sender, EventArgs e)
-    {
-      this.Close();
-      Application.Exit();
-    }
-
-  }
-
-   }
+}
 
 
