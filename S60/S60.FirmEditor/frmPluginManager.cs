09c12a6159cc12375c4d7076437e9562bc3558b4
fonix232 <fonix232@gmail.com>
2010-05-22 18:04:06 +0000

    Added proper plugin handling to the Plugin Manager

--- S60/S60.FirmEditor/frmPluginManager.cs
+++ S60/S60.FirmEditor/frmPluginManager.cs
@@ -12,22 +12,42 @@ namespace S60.FirmEditor
 {
     public partial class frmPluginManager : Form
     {
-      private List<PluginHost> m_plugins;
+        private List<PluginHost> m_plugins;
 
-      public frmPluginManager()
-      {
-        InitializeComponent();
-        m_plugins = PluginHostProvider.Plugins;
-        foreach (PluginHost loadedPlugins in m_plugins)
+        public frmPluginManager()
         {
-          TreeNode tmpNode = new TreeNode(loadedPlugins.GetPluginName());
-          treeView1.Nodes.Add(tmpNode);
-        }
-      }
+            InitializeComponent();
 
-      private void button1_Click(object sender, EventArgs e)
-      {
-        this.Close();
-      }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void pluginsLoad()
+        {
+            // Clear the treeview
+            treeView1.Nodes.Clear();
+
+            // Add root nodes
+            TreeNode firmNode = new TreeNode("Firmware plugins");
+            TreeNode fileNode = new TreeNode("File plugins");
+            treeView1.Nodes.Add(firmNode);
+            treeView1.Nodes.Add(fileNode);
+
+            // Load plugins
+            m_plugins = PluginHostProvider.Plugins;
+
+            // Go through all plugins and add them to the corresponding tree
+            foreach (PluginHost loadedPlugins in m_plugins)
+            {
+                /* if(pluginType == firmPlugin) akkor adja hozzá a FirmPlugin fához */
+                TreeNode tmpNode = new TreeNode(loadedPlugins.GetPluginName());
+
+                firmNode.Nodes.Add(tmpNode);
+                fileNode.Nodes.Add(tmpNode);
+            }
+        }
     }
 }
