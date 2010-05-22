4932e2fc6ff5d18f3d2564b939554a195dfeeff2
fonix232@gmail.com <fonix232@gmail.com@f7e02285-a4bd-d62b-a74b-6d5bd8df82f2>
2010-05-22 17:56:38 +0000

    Added property editor for further use.

--- trunk/S60/S60.CenRepEditor/Program.cs
+++ trunk/S60/S60.CenRepEditor/Program.cs
@@ -15,7 +15,7 @@ namespace S60.CenRepEditor
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmMain());
         }
     }
 }
--- trunk/S60/S60.CenRepEditor/frmMain.Designer.cs
+++ trunk/S60/S60.CenRepEditor/frmMain.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace S60.CenRepEditor
 {
-    partial class Form1
+    partial class frmMain
     {
         /// <summary>
         /// Required designer variable.
@@ -28,14 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // Form1
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(218, 127);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(484, 612);
-            this.Name = "Form1";
+            this.Controls.Add(this.button1);
+            this.Name = "frmMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Project S60 Central Repository Editor";
             this.ResumeLayout(false);
@@ -43,6 +55,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
 
