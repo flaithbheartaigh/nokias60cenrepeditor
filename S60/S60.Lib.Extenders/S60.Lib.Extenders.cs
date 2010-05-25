2c26a6bb9c78790c7e44c80ef55dafb43af70059
jakabandras210 <jakabandras210@f7e02285-a4bd-d62b-a74b-6d5bd8df82f2>
2010-05-25 16:11:40 +0000

    MIF kezelés + a szükséges bővítő metódusok az S60.Lib.Extenders-be

---
+++ S60/S60.Lib.Extenders/S60.Lib.Extenders.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Reflection;
+using System.Resources;
+
+namespace S60.Lib.Extenders
+{
+  static public class Extenders
+  {
+    static public string[] ReadAllLines(this StreamReader reader)
+    {
+      List<string> lines = new List<string>();
+      while (reader.Peek() > 0)
+      {
+        string ln = reader.ReadLine();
+        lines.Add(ln);
+      }
+      return lines.ToArray();
+    }
+    static public string[] ReadAllLines(this StreamReader reader, Func<string, bool> delFilter)
+    {
+      List<string> lines = new List<string>();
+      while (reader.Peek() > 0)
+      {
+        string ln = reader.ReadLine();
+        if (delFilter(ln))
+          lines.Add(ln);
+      }
+      return lines.ToArray();
+    }
+    public static byte[] ToByte(this string str)
+    {
+      byte[] tmp = { };
+      char[] chrtmp = str.ToCharArray();
+      int i = 0;
+      Array.Resize<byte>(ref tmp, str.Length);
+      foreach (char ch in chrtmp)
+      {
+        tmp[i] = (byte)ch;
+        i++;
+      }
+      return tmp;
+    }
+    public static byte[] ToByte(this Int32 szam)
+    {
+      byte[] tmp = { 0, 0, 0, 0 };
+      tmp[0] = (byte)(szam & 0xff);
+      tmp[1] = (byte)((szam >> 8) & 0xff);
+      tmp[2] = (byte)((szam >> 16) & (Int32)0xff);
+      tmp[3] = (byte)(szam >> 24);
+      return tmp;
+    }
+    public static byte[] Append(this byte[] x, byte[] y)
+    {
+      int oldLength = x.Length;
+      Array.Resize<byte>(ref x, oldLength + y.Length);
+      for (int i = 0; i < y.Length; i++)
+        x[oldLength + i] = y[i];
+      return x;
+    }
+
+  }
+}
