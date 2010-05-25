768a8fecadcec418befedfba8b521de48cb5cb8e
jakabandras210 <jakabandras210@f7e02285-a4bd-d62b-a74b-6d5bd8df82f2>
2010-05-25 13:11:36 +0000

    Plugin interface feltöltés

--- S60/S60.PluginInterface/IPlugin.cs
+++ S60/S60.PluginInterface/IPlugin.cs
@@ -2,10 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Xml;
 
 namespace S60.PluginInterface
 {
-    interface IPlugin
+    public interface IPlugin
     {
+      string PluginMenuName();
+      void RunPlugin();
+      void RunPlugin(XmlDocument xParams);
+      void myHandler(object sender, System.EventArgs e);
+      string GetPluginName();
     }
 }
