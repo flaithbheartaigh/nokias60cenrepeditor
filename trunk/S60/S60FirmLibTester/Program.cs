359130fd1e355bc3c1e0ae37d4a71194f407fa13
jakabandras210 <jakabandras210@f7e02285-a4bd-d62b-a74b-6d5bd8df82f2>
2010-05-27 17:41:14 +0000

    Teszt program a FirmWare lib tesztelésére

---
+++ trunk/S60/S60FirmLibTester/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using S60.Lib.Firmware;
+using System.IO;
+
+namespace S60FirmLibTester
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      FwFile myfirm = new FwFile();
+      myfirm.Test(args.Length >0 ? args[1] : Directory.GetCurrentDirectory() + @"\rofs3.fpsx");
+      UseExternalTools.ExtractImage(Directory.GetCurrentDirectory(), Directory.GetCurrentDirectory() + @"\ROFS2\6_code.bin", Directory.GetCurrentDirectory() + @"\ROFSROOT");
+    }
+  }
+}
