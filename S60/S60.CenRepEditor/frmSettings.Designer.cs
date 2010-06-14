b25899514bc3d00decc60274e562546cde6209ea
jakabandras210 <jakabandras210@f7e02285-a4bd-d62b-a74b-6d5bd8df82f2>
2010-06-14 04:12:25 +0000

    Fejlesztések a CenRep Editorban.

--- S60/S60.CenRepEditor/frmSettings.Designer.cs
+++ S60/S60.CenRepEditor/frmSettings.Designer.cs
@@ -31,7 +31,29 @@
       this.tabControl1 = new System.Windows.Forms.TabControl();
       this.tpModder = new System.Windows.Forms.TabPage();
       this.tpApplication = new System.Windows.Forms.TabPage();
+      this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+      this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+      this.label1 = new System.Windows.Forms.Label();
+      this.textBox1 = new System.Windows.Forms.TextBox();
+      this.label2 = new System.Windows.Forms.Label();
+      this.textBox2 = new System.Windows.Forms.TextBox();
+      this.label3 = new System.Windows.Forms.Label();
+      this.textBox3 = new System.Windows.Forms.TextBox();
+      this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+      this.btnCancel = new System.Windows.Forms.Button();
+      this.btnApply = new System.Windows.Forms.Button();
+      this.groupBox1 = new System.Windows.Forms.GroupBox();
+      this.groupBox2 = new System.Windows.Forms.GroupBox();
+      this.tableLayoutPanel4 = new System.Windows.Forms.TableLayoutPanel();
+      this.tableLayoutPanel5 = new System.Windows.Forms.TableLayoutPanel();
       this.tabControl1.SuspendLayout();
+      this.tpModder.SuspendLayout();
+      this.tpApplication.SuspendLayout();
+      this.tableLayoutPanel1.SuspendLayout();
+      this.tableLayoutPanel2.SuspendLayout();
+      this.tableLayoutPanel3.SuspendLayout();
+      this.groupBox1.SuspendLayout();
+      this.groupBox2.SuspendLayout();
       this.SuspendLayout();
       // 
       // tabControl1
@@ -46,6 +68,7 @@
       // 
       // tpModder
       // 
+      this.tpModder.Controls.Add(this.tableLayoutPanel1);
       this.tpModder.Location = new System.Drawing.Point(4, 22);
       this.tpModder.Name = "tpModder";
       this.tpModder.Padding = new System.Windows.Forms.Padding(3);
@@ -56,6 +79,7 @@
       // 
       // tpApplication
       // 
+      this.tpApplication.Controls.Add(this.tableLayoutPanel2);
       this.tpApplication.Location = new System.Drawing.Point(4, 22);
       this.tpApplication.Name = "tpApplication";
       this.tpApplication.Padding = new System.Windows.Forms.Padding(3);
@@ -64,16 +88,197 @@
       this.tpApplication.Text = "Application";
       this.tpApplication.UseVisualStyleBackColor = true;
       // 
+      // tableLayoutPanel1
+      // 
+      this.tableLayoutPanel1.ColumnCount = 2;
+      this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 27.00535F));
+      this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 72.99465F));
+      this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+      this.tableLayoutPanel1.Controls.Add(this.textBox1, 1, 0);
+      this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+      this.tableLayoutPanel1.Controls.Add(this.textBox2, 1, 1);
+      this.tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
+      this.tableLayoutPanel1.Controls.Add(this.textBox3, 1, 2);
+      this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 17);
+      this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+      this.tableLayoutPanel1.RowCount = 4;
+      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+      this.tableLayoutPanel1.Size = new System.Drawing.Size(374, 182);
+      this.tableLayoutPanel1.TabIndex = 0;
+      // 
+      // tableLayoutPanel2
+      // 
+      this.tableLayoutPanel2.ColumnCount = 2;
+      this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+      this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+      this.tableLayoutPanel2.Controls.Add(this.groupBox1, 0, 0);
+      this.tableLayoutPanel2.Controls.Add(this.groupBox2, 1, 0);
+      this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 19);
+      this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+      this.tableLayoutPanel2.RowCount = 1;
+      this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+      this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+      this.tableLayoutPanel2.Size = new System.Drawing.Size(377, 180);
+      this.tableLayoutPanel2.TabIndex = 0;
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(3, 0);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(86, 13);
+      this.label1.TabIndex = 0;
+      this.label1.Text = "Modder Name :";
+      // 
+      // textBox1
+      // 
+      this.textBox1.Location = new System.Drawing.Point(104, 3);
+      this.textBox1.Name = "textBox1";
+      this.textBox1.Size = new System.Drawing.Size(267, 22);
+      this.textBox1.TabIndex = 1;
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Location = new System.Drawing.Point(3, 28);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(38, 13);
+      this.label2.TabIndex = 2;
+      this.label2.Text = "EMail:";
+      // 
+      // textBox2
+      // 
+      this.textBox2.Location = new System.Drawing.Point(104, 31);
+      this.textBox2.Name = "textBox2";
+      this.textBox2.Size = new System.Drawing.Size(267, 22);
+      this.textBox2.TabIndex = 3;
+      // 
+      // label3
+      // 
+      this.label3.AutoSize = true;
+      this.label3.Location = new System.Drawing.Point(3, 56);
+      this.label3.Name = "label3";
+      this.label3.Size = new System.Drawing.Size(69, 13);
+      this.label3.TabIndex = 4;
+      this.label3.Text = "Homepage :";
+      // 
+      // textBox3
+      // 
+      this.textBox3.Location = new System.Drawing.Point(104, 59);
+      this.textBox3.Name = "textBox3";
+      this.textBox3.Size = new System.Drawing.Size(267, 22);
+      this.textBox3.TabIndex = 5;
+      // 
+      // tableLayoutPanel3
+      // 
+      this.tableLayoutPanel3.ColumnCount = 2;
+      this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+      this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+      this.tableLayoutPanel3.Controls.Add(this.btnApply, 0, 0);
+      this.tableLayoutPanel3.Controls.Add(this.btnCancel, 1, 0);
+      this.tableLayoutPanel3.Location = new System.Drawing.Point(4, 227);
+      this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+      this.tableLayoutPanel3.RowCount = 1;
+      this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+      this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+      this.tableLayoutPanel3.Size = new System.Drawing.Size(374, 37);
+      this.tableLayoutPanel3.TabIndex = 1;
+      // 
+      // btnCancel
+      // 
+      this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+      this.btnCancel.Location = new System.Drawing.Point(190, 3);
+      this.btnCancel.Name = "btnCancel";
+      this.btnCancel.Size = new System.Drawing.Size(181, 31);
+      this.btnCancel.TabIndex = 0;
+      this.btnCancel.Text = "Cancel";
+      this.btnCancel.UseVisualStyleBackColor = true;
+      // 
+      // btnApply
+      // 
+      this.btnApply.Location = new System.Drawing.Point(3, 3);
+      this.btnApply.Name = "btnApply";
+      this.btnApply.Size = new System.Drawing.Size(181, 31);
+      this.btnApply.TabIndex = 0;
+      this.btnApply.Text = "&Apply && Save";
+      this.btnApply.UseVisualStyleBackColor = true;
+      // 
+      // groupBox1
+      // 
+      this.groupBox1.Controls.Add(this.tableLayoutPanel4);
+      this.groupBox1.Location = new System.Drawing.Point(3, 3);
+      this.groupBox1.Name = "groupBox1";
+      this.groupBox1.Size = new System.Drawing.Size(182, 171);
+      this.groupBox1.TabIndex = 0;
+      this.groupBox1.TabStop = false;
+      this.groupBox1.Text = "Editing && Patching";
+      // 
+      // groupBox2
+      // 
+      this.groupBox2.Controls.Add(this.tableLayoutPanel5);
+      this.groupBox2.Location = new System.Drawing.Point(191, 3);
+      this.groupBox2.Name = "groupBox2";
+      this.groupBox2.Size = new System.Drawing.Size(183, 171);
+      this.groupBox2.TabIndex = 1;
+      this.groupBox2.TabStop = false;
+      this.groupBox2.Text = "Other";
+      // 
+      // tableLayoutPanel4
+      // 
+      this.tableLayoutPanel4.ColumnCount = 2;
+      this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+      this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+      this.tableLayoutPanel4.Location = new System.Drawing.Point(6, 21);
+      this.tableLayoutPanel4.Name = "tableLayoutPanel4";
+      this.tableLayoutPanel4.RowCount = 2;
+      this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+      this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+      this.tableLayoutPanel4.Size = new System.Drawing.Size(170, 144);
+      this.tableLayoutPanel4.TabIndex = 0;
+      // 
+      // tableLayoutPanel5
+      // 
+      this.tableLayoutPanel5.ColumnCount = 2;
+      this.tableLayoutPanel5.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+      this.tableLayoutPanel5.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+      this.tableLayoutPanel5.Location = new System.Drawing.Point(7, 21);
+      this.tableLayoutPanel5.Name = "tableLayoutPanel5";
+      this.tableLayoutPanel5.RowCount = 2;
+      this.tableLayoutPanel5.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+      this.tableLayoutPanel5.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+      this.tableLayoutPanel5.Size = new System.Drawing.Size(173, 144);
+      this.tableLayoutPanel5.TabIndex = 0;
+      // 
       // frmSettings
       // 
+      this.AcceptButton = this.btnApply;
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.CancelButton = this.btnCancel;
       this.ClientSize = new System.Drawing.Size(382, 266);
+      this.Controls.Add(this.tableLayoutPanel3);
       this.Controls.Add(this.tabControl1);
       this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+      this.HelpButton = true;
+      this.MaximizeBox = false;
+      this.MinimizeBox = false;
       this.Name = "frmSettings";
+      this.ShowIcon = false;
+      this.ShowInTaskbar = false;
       this.Text = "CenRep Editor Settings";
       this.tabControl1.ResumeLayout(false);
+      this.tpModder.ResumeLayout(false);
+      this.tpApplication.ResumeLayout(false);
+      this.tableLayoutPanel1.ResumeLayout(false);
+      this.tableLayoutPanel1.PerformLayout();
+      this.tableLayoutPanel2.ResumeLayout(false);
+      this.tableLayoutPanel3.ResumeLayout(false);
+      this.groupBox1.ResumeLayout(false);
+      this.groupBox2.ResumeLayout(false);
       this.ResumeLayout(false);
 
     }
@@ -83,5 +288,20 @@
     private System.Windows.Forms.TabControl tabControl1;
     private System.Windows.Forms.TabPage tpModder;
     private System.Windows.Forms.TabPage tpApplication;
+    private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.TextBox textBox1;
+    private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.TextBox textBox2;
+    private System.Windows.Forms.Label label3;
+    private System.Windows.Forms.TextBox textBox3;
+    private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+    private System.Windows.Forms.Button btnApply;
+    private System.Windows.Forms.Button btnCancel;
+    private System.Windows.Forms.GroupBox groupBox1;
+    private System.Windows.Forms.TableLayoutPanel tableLayoutPanel4;
+    private System.Windows.Forms.GroupBox groupBox2;
+    private System.Windows.Forms.TableLayoutPanel tableLayoutPanel5;
   }
 }
